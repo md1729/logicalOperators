a604f59ccbb298583f89d440f0ae1ad498a5d69e
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-14 13:27:19 +0300

    using logical operators
    We explained and coded with CPP in this tiny project.We shown that logical operators.

---
+++ logicalOperators.cpp
@@ -0,0 +1,30 @@
+//Logical Operators
+
+
+/*
+AND   && :Returns true(1) if the result of more than one comparison operation is true.
+OR    || :Returns true(1) if the result of one of multiple comparison operations is true.
+NOT   !  :It reverses the operation, making true false and false true.
+
+*/
+#include<iostream>
+using namespace std;
+main(){
+	/*int final =65;
+	int ortalama=80;
+	
+	cout<<(final>60 && ortalama>60); */
+	
+	int final =50;
+	int ortalama=50;
+	//int number=70;
+	int number=25;
+	bool value=1;
+	
+	//cout<<(final>60 && ortalama>60);
+	
+	// Get a number greater than 100 or less than 50 from the user
+	//cout<<(number<50||number>100);
+	cout<<(!value);
+	
+}
